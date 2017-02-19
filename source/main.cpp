146194cbad3a7d8c3811a9cfa4190987b4a82d83
Shelvacu <shelvacu@gmail.com>
2017-02-18 22:55:14 -0800

    stuff

--- source/main.cpp
+++ source/main.cpp
@@ -1,18 +1,47 @@
 #include <common.h>
+#include "spritesheets/player-with-extra.c"
+
+// The following assumes a 128x128px spritesheet with 16x16px sprites.
+#define render_sprite(spritesheet, sprite_index, x, y) ST_RenderSpritePosition((spritesheet), ((sprite_index) % 8) * 16, ((sprite_index) / 8) * 16, 16, 16, (x), (y)) 
+
+//extern const st_image player_spritesheet;
 
 int main(int argc, char **argv) {
-	int linkX = 5;
-	int linkY = 5;
-	int second = 0;
 	ST_Init();
+
+	int framecount = 0;
+
+	st_spritesheet *player_ss = ST_SpritesheetCreateSpritesheet(
+		player_spritesheet.pixel_data,
+		player_spritesheet.width,
+		player_spritesheet.height);
 	while(aptMainLoop())
 	{
-		//all the top screen stuff now
-		consoleInit(GFX_TOP, NULL);
+		ST_RenderStartFrame(GFX_TOP);
+		ST_InputScan();
 
-		//all the bottom screen stuff now
-		consoleInit(GFX_BOTTOM, NULL);
+		/*ST_RenderSpritePosition(
+			player_ss,
+			0, 0,
+			16, 16,
+			100, 100
+		);*/
+
+		int index;
+		if(framecount % 4 == 0)
+			index = 4;
+		else if(framecount % 4 == 2)
+			index = 5;
+		else
+			index = 3;
+
+		render_sprite(player_ss, index, 100, 100); 
+
+		ST_RenderEndRender();
+		framecount++;
 	}
+
+	ST_SpritesheetFreeSpritesheet(player_ss);
 	ST_Fini();
 	return 0;
 }
